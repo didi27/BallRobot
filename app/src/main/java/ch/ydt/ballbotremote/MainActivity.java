c4ee39e9beacec40cf832b2c4fa3d6ae2c03cb74
Yannick Dietler <ydietler@gmail.com>
2016-03-01 15:37:28 +0100

    init

--- app/src/main/java/ch/ydt/ballbotremote/MainActivity.java
+++ app/src/main/java/ch/ydt/ballbotremote/MainActivity.java
@@ -1,7 +1,13 @@
 package ch.ydt.ballbotremote;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.buttonforwards);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
